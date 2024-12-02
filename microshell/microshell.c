e7c6f2395ac06aea675e189088e8f09075a427c8
root <sushi.honda@gmail.com>
2024-12-02 09:46:52 +0800

    11/24 try again incomplete
     several if conditions in main()
     try to use fd_r
     ...

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -1,55 +1,36 @@
+#include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
-
-void	ft_putstr_fd2(char *str, char *av)
-{
-	while (*str)
-		write (2, str++, 1);
-	if (av)
-		while (*av)
-			write (2, av++, 1);
-	write (2, "\n", 1);
-}
 
 void	ft_execute(char *av[], int i, int fd_r, char *envp[])
 {
-	av[i] = NULL;
-	execve(av[0], av, envp);
-	ft_putstr_fd2("error: cannot execute ", av[0]);
-	exit (1);
+
 }
 
-int	main(int ac, char **av, char **envp)
+int	main(int ac, char **av, char ** envp)
 {
 	int		i;
-	int		fd_r;
-	int		pid;
 
 	i = 0;
-	av = &av[i + 1];
-	//fd_r = dup(STDIN_FILENO);
 	while (av[i] && av[i + 1])
 	{
+		i = 0;
+		av = &av[i + 1];
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
+			i++;
 		if (i != 0 && (!av[i] || !strcmp(av[i], ";")))
 		{
-			pid = fork();
-			if (pid == 0)
-				ft_execute(av, i, fd_r, envp);
-			else
+			if (fork() == 0)
 			{
-				//close(fd_r);
-				while (waitpid(-1, NULL, WUNTRACED) != -1)
-					;
-				//fd_r = dup(STDIN_FILENO);
+				ft_execute(av, i, fd_r, envp);
 			}
-			av = &av[i + 1];
-			i = 0;
+			else
+				close(fd_r);
+				while (waitpid(-1, NULL, WUNTRACE) != -1)
+					;
+				fd_r = dup(STDIN_FILENO);
 		}
-		else
-			i++;
 	}
-	//close(fd_r);
 	return (0);
 }
