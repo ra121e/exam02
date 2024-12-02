2b9921351801f2bf3e74ef03fd3cc4a8c037196f
root <sushi.honda@gmail.com>
2024-12-02 10:21:27 +0800

    11/27 success without WIFEXITED
     arg, av[0] for command

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -1,11 +1,9 @@
-#include <sys/wait.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
-#define WRITE_END 1
-#define READ_END 0
 
-void	err_exit(char *str)
+void	err(char *str)
 {
 	while (*str)
 		write(2, str++, 1);
@@ -15,86 +13,90 @@ int	builtin_cd(char *av[], int i)
 {
 	if (i != 2)
 	{
-		err_exit("error: cd: bad arguments\n");
+		err("error: cd: bad arguments\n");
 		return (1);
 	}
 	if (chdir(av[1]) == -1)
 	{
-		err_exit("error: cd\n");
+		err("error: cd: cannot change directory ");
+		err(av[1]);
+		err("\n");
 		return (1);
 	}
 	return (0);
 }
 
-void	set_fd(int to_pipe, int *fd, int end)
+void	set_fd(int to_pipe, int fd[2], int end)
 {
-	if (to_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+	if (to_pipe)
 	{
-		err_exit("error: dup\n");
-		exit(1);
+		if (dup2(fd[end], end) == -1)
+		{
+			err("error: fatal\n");
+			exit(1);
+		}
+		if (close(fd[0]) == -1)
+		{
+			err("error: fatal\n");
+			exit(1);
+		}
+		if (close(fd[1]) == -1)
+		{
+			err("error: fatal\n");
+			exit(1);
+		}
 	}
 }
-//void	set_fd(int to_pipe, int fd[2], int end)
-//{
-//	if (to_pipe)
-//	{
-//		dup2(fd[end], end);
-//		close(fd[0]);
-//		close(fd[1]);
-//	}
-//}
 
-int	exec(char *av[], int i, char *envp[])
+int	exec(char **av, int i, char **envp)
 {
-	int	to_pipe;
-	int	fd[2];
 	int	pid;
 	int	status;
+	int to_pipe;
+	int	fd[2];
 
+	to_pipe = 0;
 	if (av[i] && !strcmp(av[i], "|"))
+	{
 		to_pipe = 1;
-	else
-		to_pipe = 0;
+	}
 	if (!to_pipe && !strcmp(av[0], "cd"))
 	{
-		return(builtin_cd(av, i));
+		status = builtin_cd(av, i);
+		return (status);
 	}
-	if (to_pipe && pipe(fd) == -1)
+	if (to_pipe)
 	{
-		err_exit("error: pip\n");
-		exit(1);
+		if (pipe(fd) == -1)
+		{
+			err("error: fatal\n");
+			exit(1);
+		}
 	}
-//	if (to_pipe)
-//	{
-//		if (pipe(fd) == -1)
-//		{
-//			err_exit("error: pipe \n");
-//			exit(1);
-//		}
-//	}
 	pid = fork();
-	if (pid == -1)
+	if (pid < 0)
 	{
-		err_exit("error: fork\n");
+		err("error: fatal\n");
 		exit(1);
 	}
-	if (pid == 0)
+	if (!pid)
 	{
 		av[i] = 0;
-		set_fd(to_pipe, fd, WRITE_END);
+		set_fd(to_pipe, fd, 1);
 		if (!strcmp(av[0], "cd"))
 		{
-			exit(builtin_cd(av, i));
+			status = builtin_cd(av, i);
+			exit (status);
 		}
-		execve(*av, av, envp);
-		err_exit("error: cannot execute ");
-		err_exit(*av);
-		err_exit("\n");
+		execve(av[0], av, envp);
+		err("error: cannot execute ");
+		err(av[0]);
+		err("\n");
 		exit(1);
 	}
 	waitpid(pid, &status, 0);
-	set_fd(to_pipe, fd, READ_END);
-	return (WIFEXITED(status) && WEXITSTATUS(status));
+	set_fd(to_pipe, fd, 0);
+	return (WEXITSTATUS(status));
 }
 
 int	main(int ac, char **av, char **envp)
@@ -113,6 +115,7 @@ int	main(int ac, char **av, char **envp)
 			i++;
 		if (i)
 			status = exec(av, i, envp);
+
 	}
 	return (status);
 }
