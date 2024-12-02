116923835c8d64ce72da478e271ff1b03220fd90
root <sushi.honda@gmail.com>
2024-12-02 10:18:32 +0800

    11/26 try own function but errors
     error reason is message11/26 try own function but errors
    
      error reason is message11/26 try own function but errors
    
       error reason is message11/26 try own function but errors
    
        error reason is message11/26 try own function but errors
    
    	 error reason is message11/26 try own function but errors
    
    	  error reason is message11/26 try own function but errors
    
    	   error reason is message11/26 try own function but errors
    
    	    error reason is message11/26 try own function but errors
    
    		 error reason is message11/26 try own function but errors
    
    		  error reason is message11/26 try own function but errors
    
    		   error reason is message

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -1,72 +1,99 @@
-#include <stdlib.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#define WRITE_END 1
+#define READ_END 0
 
-void	error(char *str)
+void	err_exit(char *str)
 {
 	while (*str)
-		write (2, str++, 1);
+		write(2, str++, 1);
 }
 
-int	cd(char **av, int i)
+int	builtin_cd(char *av[], int i)
 {
 	if (i != 2)
 	{
-		error ("error: cd: bad args\n");
+		err_exit("error: cd: bad arguments\n");
 		return (1);
 	}
 	if (chdir(av[1]) == -1)
 	{
-		error ("error: cd: cannot change directory to \n");
+		err_exit("error: cd\n");
 		return (1);
 	}
 	return (0);
 }
 
-void	set_pipe(int flag_pipe, int *fd, int end)
+void	set_fd(int to_pipe, int *fd, int end)
 {
-	if (flag_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+	if (to_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 	{
-		error("error: dup\n");
+		err_exit("error: dup\n");
 		exit(1);
 	}
 }
+//void	set_fd(int to_pipe, int fd[2], int end)
+//{
+//	if (to_pipe)
+//	{
+//		dup2(fd[end], end);
+//		close(fd[0]);
+//		close(fd[1]);
+//	}
+//}
 
-int	exec(char **av, int i, char **envp)
+int	exec(char *av[], int i, char *envp[])
 {
-	int	flag_pipe;
+	int	to_pipe;
 	int	fd[2];
 	int	pid;
 	int	status;
 
-	flag_pipe = av[i] && !strcmp(av[i], "|");
-	if (!flag_pipe && !strcmp(*av, "cd"))
+	if (av[i] && !strcmp(av[i], "|"))
+		to_pipe = 1;
+	else
+		to_pipe = 0;
+	if (!to_pipe && !strcmp(av[0], "cd"))
 	{
-		return (cd(av, i));
+		return(builtin_cd(av, i));
 	}
-	if (flag_pipe && pipe(fd) == -1)
+	if (to_pipe && pipe(fd) == -1)
 	{
-		error("error: pip\n");
+		err_exit("error: pip\n");
 		exit(1);
 	}
-	if ((pid = fork()) == -1)
+//	if (to_pipe)
+//	{
+//		if (pipe(fd) == -1)
+//		{
+//			err_exit("error: pipe \n");
+//			exit(1);
+//		}
+//	}
+	pid = fork();
+	if (pid == -1)
 	{
-		error("error: fork\n");
+		err_exit("error: fork\n");
 		exit(1);
 	}
-	if (!pid)
+	if (pid == 0)
 	{
 		av[i] = 0;
-		set_pipe(flag_pipe, fd, 1);
-		if (!strcmp(*av, "cd"))
-			exit(cd(av, i));
+		set_fd(to_pipe, fd, WRITE_END);
+		if (!strcmp(av[0], "cd"))
+		{
+			exit(builtin_cd(av, i));
+		}
 		execve(*av, av, envp);
-		error("error: cannot execute\n");
+		err_exit("error: cannot execute ");
+		err_exit(*av);
+		err_exit("\n");
 		exit(1);
 	}
 	waitpid(pid, &status, 0);
-	set_pipe(flag_pipe, fd, 0);
+	set_fd(to_pipe, fd, READ_END);
 	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
 
