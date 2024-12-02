0b52609ae5fa581984d19ea541a848b98a0e8e05
root <sushi.honda@gmail.com>
2024-12-02 10:47:09 +0800

    11/7 based on NJ

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,125 +1,128 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
-int	ft_putchar(char c, int i)
+int	ft_putchar(char c, int len)
 {
 	write(1, &c, 1);
-	i++;
-	return (i);
+	len++;
+	return (len);
 }
 
-int	ft_found_x(int x, int i)
+int	ft_puthexa(int x, int len)
 {
 	unsigned int	u;
 	char			*hexabase;
 
-	hexabase = "0123456789abcdef";
 	u = (unsigned int)x;
-	if (u == 0)
-	{
-		write(1, "0", 1);
-		i = i + 1;
-	}
-	else if (u <= 15)
-	{
-		i = ft_putchar(hexabase[u], i);
-	}
+	hexabase = "0123456789abcdef";
+	if (u <= 15)
+		len = ft_putchar(hexabase[u], len);
 	else
 	{
-		i = ft_found_x(u / 16, i);
-		i = ft_putchar(hexabase[u % 16], i);
+		len = ft_puthexa(u / 16, len);
+		len = ft_putchar(hexabase[u % 16], len);
 	}
-	return (i);
+	return (len);
 }
 
-int	ft_found_d(int d, int i)
+int	ft_putnbr(int d, int len)
 {
 	if (d == -2147483648)
 	{
 		write(1, "-2147483648", 11);
-		i = i + 11;
+		len = len + 11;
 	}
 	else if (d < 0)
 	{
-		write(1, "-", 1);
-		i++;
-		i = ft_found_d(-d, i);
+		len = ft_putchar('-', len);
+		len = ft_putnbr(-d, len);
+	}
+	else if (d == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		len = len + 11;
 	}
 	else if (d >= 0 && d <= 9)
 	{
-		i = ft_putchar(d + '0', i);
+		len = ft_putchar(d % 10 + '0', len);
 	}
 	else
 	{
-		i = ft_found_d(d / 10, i);
-		i = ft_putchar(d % 10 + '0', i);
+		len = ft_putnbr(d / 10, len);
+		len = ft_putchar(d % 10 + '0', len);
 	}
-	return (i);
+	return (len);
 }
-int	ft_found_s(char *s, int i)
+
+int	ft_putstr(char *str, int len)
 {
-	if (s == NULL)
-		i = ft_found_s("(null)", i);
-	else
+	if (!str)
 	{
-		while (*s)
-		{
-			write(1, &(*s), 1);
-			i++;
-			s++;
-		}
+		write(1,"(null)", 6);
+		len = len + 6;
+	}	
+	while (str && *str)
+	{
+		len = ft_putchar(*str, len);
+		str++;
 	}
-	return (i);
+	return (len);
 }
-int	ft_next_percent(char *format, va_list ap, int i, int j)
+
+int	filter(va_list ap, char c, int len)
 {
-	if (format[j] == 's')
-		i = ft_found_s(va_arg(ap, char *), i);
-	else if (format[j] == 'd')
-	{
-		i = ft_found_d(va_arg(ap, int), i);
-	}
-	else if (format[j] == 'x')
-	{
-		i = ft_found_x(va_arg(ap, int), i);
-	}
-	return (i);
+	if (c == 's')
+		len = ft_putstr(va_arg(ap, char *), len);
+	else if (c == 'd')
+		len = ft_putnbr(va_arg(ap, int), len);
+	else if (c == 'x')
+		len = ft_puthexa(va_arg(ap, int), len);
+	return (len);
 }
+
 int	ft_printf(char *format, ...)
 {
 	va_list	ap;
 	int		i;
-	int		j;
-
+	int		len;
+	
+	len = 0;
 	i = 0;
-	j = 0;
 	va_start(ap, format);
-	while (format[j])
+	while (format[i])
 	{
-		if (format[j] == '%')
+		if (format[i] == '%')
 		{
-			i = ft_next_percent(format, ap, i, ++j);
+			len = filter(ap, format[i + 1], len);
+			i++;
 		}
 		else
 		{
-			write(1, &format[j], 1);
-			i++;
+			write(1, &format[i], 1);
+			len++;
 		}
-		j++;
+		i++;
 	}
 	va_end(ap);
-	return (i);
+	return (len);
 }
 /*
 int	main(void)
 {
-	int	i;
+	char	s[] = "";
+	int		d;
+	int		x;
+	int		len;
 
-	char	str[] = "hohoho";
-	printf("printf: hello 42\n: s: %s, d: %d x: %x\n", str, -42, 42);
-	ft_printf("ft_printf: hello 42\n: s: %s, d: %d x: %x\n", str, -42, 42);
+	d = 42;
+	x = -42;
+	len = 0;
+	
+	len = printf("s: %s d: %d x: %x\n", NULL, d, x);
+	printf("original length: %d\n", len);
+	len = ft_printf("s: %s d: %d x: %x\n", NULL, d, x);
+	printf("ft length: %d\n", len);
 	return (0);
 }
 */
