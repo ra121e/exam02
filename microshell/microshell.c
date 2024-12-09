6ca82a83dbf34409103aa2a3219f87f27f9eef7c
root <sushi.honda@gmail.com>
2024-12-09 10:54:45 +0800

    12/9 microshell
     i = 0 in while loop

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -1,5 +1,5 @@
-#include <sys/wait.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 
@@ -7,6 +7,23 @@ void	err(char *str)
 {
 	while (*str)
 		write(2, str++, 1);
+}
+
+int	builtin_cd(char **av, int i)
+{
+	if (i != 2)
+	{
+		err("error: cd: bad arguments\n");
+		return (1);
+	}
+	if (chdir(av[1]) == -1)
+	{
+		err("error: cd: cannot change directory to ");
+		err(av[1]);
+		err("\n");
+		return (1);
+	}
+	return (0);
 }
 
 void	set_fd(int to_pipe, int fd[2], int end)
@@ -31,30 +48,14 @@ void	set_fd(int to_pipe, int fd[2], int end)
 	}
 }
 
-int	builtin_cd(char **av, int i)
-{
-	if (i != 2)
-	{
-		err("error: cd: bad arguments\n");
-		return (1);
-	}
-	if (chdir(av[1]) == -1)
-	{
-		err("error: cd: cannot change directory ");
-		err(av[1]);
-		err("\n");
-		return (1);
-	}
-	return (0);
-}
-
 int	exec(char **av, int i, char **envp)
 {
+	int	status;
 	int	pid;
 	int	fd[2];
 	int	to_pipe;
-	int	status;
 
+	status = 0;
 	to_pipe = 0;
 	if (av[i] && !strcmp(av[i], "|"))
 		to_pipe = 1;
@@ -63,10 +64,13 @@ int	exec(char **av, int i, char **envp)
 		status = builtin_cd(av, i);
 		return (status);
 	}
-	if (pipe(fd) == -1)
+	if (to_pipe == 1)
 	{
-		err("error: fatal\n");
-		exit(1);
+		if (pipe(fd) == -1)
+		{
+			err("error: fatal\n");
+			exit(1);
+		}
 	}
 	pid = fork();
 	if (pid < 0)
@@ -91,8 +95,8 @@ int	exec(char **av, int i, char **envp)
 
 int	main(int ac, char **av, char **envp)
 {
-	int	i;
 	int	status;
+	int	i;
 
 	(void)ac;
 	status = 0;
