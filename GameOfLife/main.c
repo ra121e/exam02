af12bb93d3f2fc11fb2ec1ff629ced467e707988
Atsushi Honda <athonda@c1r8s1.42singapore.sg>
2025-07-13 15:16:34 +0800

    GOL: 3rd
    forgets
      - free(next) in iterator()

--- GameOfLife/main.c
+++ GameOfLife/main.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int	parse(char **av, int *width, int *height, int *iteration)
 {
@@ -10,23 +10,20 @@ int	parse(char **av, int *width, int *height, int *iteration)
 
 	if (w <= 0 || h <= 0 || i < 0)
 		return (1);
-	else
-	{
-		*width = w;
-		*height = h;
-		*iteration = i;
-	}
+	*width = w;
+	*height = h;
+	*iteration = i;
 	return (0);
 }
 
 char	**make_board(int width, int height)
 {
-	char	**board;
+	char **board;
 
 	board = (char**)malloc(sizeof(char*) * height);
 	if (board == NULL)
 		return (NULL);
-	for (int i = 0; i < height; ++i)
+	for (int i = 0; i < width; ++i)
 	{
 		board[i] = (char*)calloc(width, sizeof(char));
 		if (board[i] == NULL)
@@ -40,19 +37,18 @@ char	**make_board(int width, int height)
 	return (board);
 }
 
-
 void	draw(char **board, int width, int height)
 {
 	char	c;
 	int		x = 0, y = 0;
 	bool	pen = false;
 
-	while(read(0, &c, 1))
+	while (read(0, &c, 1))
 	{
 		if (c == 'a' && x > 0)
 			x--;
 		else if (c == 'w' && y > 0)
-			y--;
+			y--;			
 		else if (c == 'd' && x < width - 1)
 			x++;
 		else if (c == 's' && y < height - 1)
@@ -60,7 +56,7 @@ void	draw(char **board, int width, int height)
 		else if (c == 'x')
 			pen = !pen;
 		else
-			continue ;
+			continue;
 		if (pen)
 			board[y][x] = 1;
 	}
@@ -72,10 +68,10 @@ void	print_board(char **board, int width, int height)
 	{
 		for (int x = 0; x < width; ++x)
 		{
-			if (board[y][x] == 1)
-				write (1, "0", 1);
+			if (board[y][x])
+				write(1, "0", 1);
 			else
-				write (1, " ", 1);
+				write(1, " ", 1);
 		}
 		write(1, "\n", 1);
 	}
@@ -90,22 +86,19 @@ void	free_board(char **board, int height)
 
 int	count(char **board, int width, int height, int x, int y)
 {
-	int	count = 0;
+	int count = 0;
 
 	for (int i = -1; i <= 1; ++i)
 	{
 		for (int j = -1; j <= 1; ++j)
 		{
 			if (i == 0 && j == 0)
-				continue ;
-			else
-			{
-				int ny = y + i;
-				int nx = x + j;
-				if (ny >= 0 && nx >= 0 && ny < height && nx < width)
-					if (board[ny][nx] == 1)
-						count++;
-			}
+				continue;
+			int ny = y + i;
+			int nx = x + j;
+			if (ny >=0 && nx >= 0 && ny < height && nx < width)
+				if (board[ny][nx] == 1)
+					count++;
 		}
 	}
 	return (count);
@@ -113,23 +106,21 @@ int	count(char **board, int width, int height, int x, int y)
 
 char	**iterator(char **board, int width, int height, int iteration)
 {
-	int 	sum = 0;
 	char	**next;
-
 	next = make_board(width, height);
-	for (int n = 0; n < iteration; ++n)
+	for (int i = 0; i < iteration; ++i)
 	{
 		for (int y = 0; y < height; ++y)
 		{
 			for (int x = 0; x < width; ++x)
 			{
-				sum = count(board, width, height, x, y);
+				int sum = count(board, width, height, x, y);
 				if (board[y][x] && (sum == 2 || sum == 3))
 					next[y][x] = 1;
 				else if (!board[y][x] && sum == 3)
 					next[y][x] = 1;
 				else
-					next[y][x] =0;
+					next[y][x] = 0;
 			}
 		}
 		char **tmp;
@@ -143,25 +134,24 @@ char	**iterator(char **board, int width, int height, int iteration)
 
 int	main(int ac, char **av)
 {
-	int	width, height, iteration;
-	char **board;
+	int width, height, iteration;
 
 	if (ac != 4)
 		return (1);
 	if (parse(av, &width, &height, &iteration))
 		return (1);
-	board = make_board(width, height);
-	if (board ==NULL)
+	char **board = make_board(width, height);
+	if (board == NULL)
 		return (1);
 	draw(board, width, height);
 	if (iteration == 0)
 	{
 		print_board(board, width, height);
-		free_board(board,height);
+		free_board(board, height);
 	}
 	else
 	{
-		char	**last = iterator(board, width, height, iteration);
+		char **last = iterator(board, width, height, iteration);
 		print_board(last, width, height);
 		free_board(last, height);
 	}
